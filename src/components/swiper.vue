ede6b61e742863d8c5185d293c8aeadca68f11fe
ssx <1554816265@qq.com>
2020-05-04 21:48:26 +0800

    susie cm

--- src/components/swiper.vue
+++ src/components/swiper.vue
@@ -3,20 +3,24 @@
         <!-- 只有一张的时候，只显示，不做任何操作 -->
         <div v-if="list.length===1">
             <div v-for="(li,index) in list" :style="{'width':listWidth+'%'}" :key="index">
-                <p style="height: 150px;line-height:150px;background-color: dodgerblue;text-align: center;">{{index+1}}</p>
+                <p style="height: 150px;line-height:150px;background-color: dodgerblue;text-align: center;">
+                    {{index+1}}</p>
             </div>
         </div>
         <!-- 左右滑动-->
-        <div class="mui-swipe__track" :style="[{width:ulWidth},{'transform':'translateX(-'+(listWidth*(nowIndex+1))+'%)'}]"
+        <div class="mui-swipe__track"
+             :style="[{width:ulWidth},{'transform':'translateX(-'+(listWidth*(nowIndex+1))+'%)'}]"
              v-if="list.length >1"
              @touchstart="touchStar" @touchend="touchEnd"
              :class="{'mui-swipe--tran':noLast}">
             <!--最后一张-->
             <div :style="{'width':listWidth+'%'}">
-                <p style="height: 150px;line-height:150px;background-color: dodgerblue;text-align: center;">{{list.length}}</p>
+                <p style="height: 150px;line-height:150px;background-color: dodgerblue;text-align: center;">
+                    {{list.length}}</p>
             </div>
-            <div v-for="(li,index) in list" :style="{'width':listWidth+'%'}" :key="index" >
-                <p style="height: 150px;line-height:150px;background-color: dodgerblue;text-align: center;">{{index+1}}</p>
+            <div v-for="(li,index) in list" :style="{'width':listWidth+'%'}" :key="index">
+                <p style="height: 150px;line-height:150px;background-color: dodgerblue;text-align: center;">
+                    {{index+1}}</p>
             </div>
             <!--第一张-->
             <div :style="{'width':listWidth+'%'}">
@@ -25,9 +29,9 @@
         </div>
         <!--如果需要显示对应的点-->
         <div class="mui-swipe__indicators" v-if="option && list.length >1">
-            <i class="mui-swipe__indicator mui-swipe__indicator--active" v-if="nowIndex===list.length"></i>
-            <i v-for="(li,index) in list" :key="index" :class="{'mui-swipe__indicator':true,'mui-swipe__indicator--active':index===nowIndex}" :style="index===nowIndex ? indicatorStyle : null"></i>
-            <i class="mui-swipe__indicator mui-swipe__indicator--active" v-if="nowIndex===-1"></i>
+            <i v-for="(li,index) in list" :key="index"
+               :class="{'mui-swipe__indicator':true,'mui-swipe__indicator--active':index===nowIndex}"
+               :style="index===nowIndex ? indicatorStyle : null"></i>
         </div>
     </div>
 </template>
@@ -72,9 +76,9 @@
                 nowIndex: 0,
                 timer: null,
                 noLast: true,
-                startX:0,
-                startY:0,
-                test:true
+                startX: 0,
+                startY: 0,
+                test: true
             };
         },
         computed: {
@@ -83,8 +87,8 @@
                 return (this.list.length + 2) + "00%";
             },
             //li宽度
-            listWidth:function () {
-                return 100/(this.list.length+2)
+            listWidth: function () {
+                return 100 / (this.list.length + 2)
             },
             indicatorStyle() {
                 return {
@@ -101,88 +105,88 @@
         },
         methods: {
             //滑动操作
-            switchDo(reduce){
+            switchDo(reduce) {
                 clearInterval(this.timer);
                 //根据reduce判断this.nowIndex的增加或者减少！
-                if(reduce==='reduce'){
-                    if(this.nowIndex===0){
+                if (reduce === 'reduce') {
+                    if (this.nowIndex === 0) {
                         //如果是滑动切换
-                        if(this.type==='slide'){
+                        if (this.type === 'slide') {
                             this.nowIndex--;
                             //执行完了这次动画之后，去除过渡效果
                             setTimeout(() => {
-                                this.nowIndex = this.list.length-1;
+                                this.nowIndex = this.list.length - 1;
                                 this.noLast = false;
                             }, 400)
                         }
-                        else{
-                            this.nowIndex = this.list.length-1;
+                        else {
+                            this.nowIndex = this.list.length - 1;
                         }
                     }
-                    else{
+                    else {
                         this.nowIndex--;
                     }
                 }
-                else{
+                else {
                     this.nowIndex++;
                 }
                 if (this.nowIndex === this.list.length) {
-                    if(this.type==='slide') {
+                    if (this.type === 'slide') {
                         //执行完了这次动画之后，去除过渡效果
                         setTimeout(() => {
                             this.nowIndex = 0;
                             this.noLast = false;
                         }, 400)
                     }
-                    else{
+                    else {
                         this.nowIndex = 0;
                     }
                     // this.nowIndex = 0;
                 }
 
                 //是否显示图片，只针对透明度切换的情况！
-                setTimeout(()=>{
-                    this.nowIndexShow=this.nowIndex;
-                },1)
+                setTimeout(() => {
+                    this.nowIndexShow = this.nowIndex;
+                }, 1)
                 //如果需要自动播放
                 if (this.autoplay) {
                     this.autoSwitch();
                 }
                 //如果是滑动切换，设置this.noLast，增加过渡效果
-                if(this.type==='slide') {
+                if (this.type === 'slide') {
                     this.noLast = true;
                 }
 
             },
             //自动播放函数
-            autoSwitch(){
+            autoSwitch() {
                 let time = this.time || 4000;
                 this.timer = setInterval(() => {
                     this.switchDo();
                 }, time);
             },
             //开始滑动
-            touchStar(e){
+            touchStar(e) {
                 // e.preventDefault();
-                this.startX=e.changedTouches[0].clientX;
-                this.startY=e.changedTouches[0].clientY;
+                this.startX = e.changedTouches[0].clientX;
+                this.startY = e.changedTouches[0].clientY;
             },
             //滑动结束
-            touchEnd(e){
+            touchEnd(e) {
                 //e.preventDefault();
-                if(this.vertical){
-                    if(e.changedTouches[0].clientY-this.startY>50){
+                if (this.vertical) {
+                    if (e.changedTouches[0].clientY - this.startY > 50) {
                         this.switchDo('reduce')
                     }
-                    else if(e.changedTouches[0].clientY-this.startY<-50){
+                    else if (e.changedTouches[0].clientY - this.startY < -50) {
                         this.switchDo()
                     }
                 }
-                else{
-                    if(e.changedTouches[0].clientX-this.startX>50){
+                else {
+                    if (e.changedTouches[0].clientX - this.startX > 50) {
                         this.switchDo('reduce')
                     }
-                    else if(e.changedTouches[0].clientX-this.startX<-50){
+                    else if (e.changedTouches[0].clientX - this.startX < -50) {
                         this.switchDo()
                     }
                 }
@@ -191,7 +195,7 @@
     };
 </script>
 <style lang="css">
-    .mui-swipe{
+    .mui-swipe {
         width: 100%;
         height: 100%;
         margin-left: auto;
@@ -204,14 +208,17 @@
         padding: 0;
         z-index: 1;
     }
-    .mui-swipe__track{
+
+    .mui-swipe__track {
         display: flex;
         height: 100%;
     }
-    .mui-swipe--tran{
+
+    .mui-swipe--tran {
         transition: all .4s;
     }
-    .mui-swipe__indicators{
+
+    .mui-swipe__indicators {
         position: absolute;
         bottom: 24px;
         left: 50%;
@@ -227,9 +234,11 @@
         opacity: 1;
         transition: opacity 0.2s;
     }
+
     .mui-swipe__indicator:not(:last-child) {
         margin-right: 6px;
     }
+
     .mui-swipe__indicator--active {
         background-color: #fff;
         opacity: 1;
